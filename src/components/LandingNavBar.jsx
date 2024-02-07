1abeff8e9d38644428cb99259d32c3d3e8ed32c3
BlessedSwajwal <nospamsajal@gmail.com>
2024-02-07 17:17:36 +0545

    Added footer

--- src/components/LandingNavBar.jsx
+++ src/components/LandingNavBar.jsx
@@ -7,6 +7,7 @@ import {
   IconButton,
   Menu,
   MenuItem,
+  Stack,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -15,7 +16,16 @@ import styled from "@emotion/styled";
 import { Link, Outlet, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { useAuth } from "../hooks/auth";
-import { Person } from "@mui/icons-material";
+import {
+  AddIcCall,
+  Email,
+  Facebook,
+  LinkedIn,
+  Person,
+  Place,
+  X,
+  YouTube,
+} from "@mui/icons-material";
 
 function LandingNavBar() {
   const isLoggedIn = useAuth();
@@ -143,6 +153,76 @@ function LandingNavBar() {
       <>
         <Outlet />
       </>
+      <Box display="flex" alignItems="center" justifyContent="center">
+        <Box
+          width="100%"
+          display="flex"
+          flexWrap="wrap"
+          bgcolor="gray"
+          pb="5vh"
+          paddingInline="3vw"
+        >
+          <Box sx={{ color: "white", padding: 3 }} flexBasis="600px">
+            <Typography variant="h5" fontWeight={700}>
+              Escrow Service
+            </Typography>
+            <Typography variant="p" lineHeight={1.5}>
+              An escrow service acts as a neutral third party in a financial
+              transaction. It holds onto the funds and assets involved in the
+              transaction until all parties has fulfilled their obligations and
+              the transaction is completed.
+            </Typography>
+          </Box>
+          <Stack
+            direction="row"
+            flex={1}
+            display="flex"
+            flexWrap="wrap"
+            justifyContent="flex-start"
+          >
+            <Box p={3} color="white">
+              <Typography fontSize={20} fontWeight={700}>
+                Contact Us
+              </Typography>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "10px" }}
+              >
+                <Place />
+                <p>Kathmandu, Nepal</p>
+              </div>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "10px" }}
+              >
+                <AddIcCall />
+                <p>+977 01-123456</p>
+              </div>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "10px" }}
+              >
+                <Email />
+                <p>info@escrow.com</p>
+              </div>
+            </Box>
+            <Box p={3} color="white">
+              <Typography fontSize={20} fontWeight={700}>
+                Follow Us
+              </Typography>
+              <p>
+                Stay updated with our latest news and updates by following us on
+                social media
+              </p>
+              <div
+                style={{ display: "flex", alignItems: "center", gap: "20px" }}
+              >
+                <Facebook />
+                <X />
+                <YouTube />
+                <LinkedIn />
+              </div>
+            </Box>
+          </Stack>
+        </Box>
+      </Box>
     </>
   );
 }
