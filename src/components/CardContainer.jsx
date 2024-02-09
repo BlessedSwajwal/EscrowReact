945fbe90db0a9c98b23dd86f9729cd074d4b2455
BlessedSwajwal <nospamsajal@gmail.com>
2024-02-09 11:14:28 +0545

    Hosted backend

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 import { Grid, Paper, Typography } from "@mui/material";
 import talent from "../assets/talent.png";
+import freelance from "../assets/freelance.png";
+import business from "../assets/business.png";
 import { PropTypes } from "prop-types";
 
 function CardContainer() {
@@ -14,10 +16,11 @@ function CardContainer() {
     width: "100%",
   }));
 
-  const CardItem = ({ heading, description }) => (
+  // eslint-disable-next-line react/prop-types
+  const CardItem = ({ heading, description, image }) => (
     <Grid item xs={12} sm={6} md={4}>
       <Item>
-        <img src={talent} alt="talent" height="100px" />
+        <img src={image} alt="talent" height="100px" />
         <Typography variant="h5">{heading}</Typography>
         <Typography textAlign="left">{description}</Typography>
       </Item>
@@ -35,18 +38,20 @@ function CardContainer() {
         description=" We open the magic portal for you to gain find talents from all over
           the world. A decentralized service where freelancers from all over the
           world are welcome to compete for orders."
+        image={talent}
       />
       <CardItem
-        heading="Global Talents"
-        description=" We open the magic portal for you to gain find talents from all over
-          the world. A decentralized service where freelancers from all over the
-          world are welcome to compete for orders."
+        heading="Work with the best"
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea deserunt incidunt, 
+        consectetur perspiciatis ad eos praesentium excepturi, autem accusantium id ratione? "
+        image={freelance}
       />
+
       <CardItem
         heading="Global Talents"
-        description=" We open the magic portal for you to gain find talents from all over
-          the world. A decentralized service where freelancers from all over the
-          world are welcome to compete for orders."
+        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ea deserunt incidunt, 
+        consectetur perspiciatis ad eos praesentium excepturi, autem accusantium id ratione?"
+        image={business}
       />
     </Grid>
   );
